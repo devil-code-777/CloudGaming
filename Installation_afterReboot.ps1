cb3877b2ff972e5ab0757c009ad70157291fb1df
devil-code-777 <76907920+devil-code-777@users.noreply.github.com>
2021-01-03 13:45:23 +0000

    Installation scripts added

---
+++ Installation_afterReboot.ps1
@@ -0,0 +1,83 @@
+﻿<# Set Variables for Download URLs #>
+$VCRedistLocation = "https://aka.ms/vs/16/release/vc_redist.x86.exe"  
+$TightVNCLocation = "https://www.tightvnc.com/download/2.8.59/tightvnc-2.8.59-gpl-setup-64bit.msi"  
+$SteamLocation = "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe"  
+$OriginLocation = "https://www.dm.origin.com/download"  
+
+<# Set VM Resolution #>
+$Screenwidth = 1920
+$Screenwidth = Read-Host "Set your Screen width (Pixels) - default 1920"
+$Screenheight = 1080
+$Screenheight = Read-Host "Set your Screen height (Pixels) - default 1080"
+Write-host "You have set the screen resolution to $Screenwidth x $Screenheight. This will be reapplied at every startup."
+
+
+<# Once Graphics driver is installed: Update Registry #>
+"Graphics Driver Installed?"
+$ReadHost = Read-Host "(Y/N)"
+Switch ($ReadHost){
+    Y {
+         Write-host "Setting up Windows Registry Entries for NVidia Gaming Drivers"
+         New-ItemProperty -Path "HKLM:\SOFTWARE\NVIDIA Corporation\Global" -Name "vGamingMarketplace" -PropertyType "DWord" -Value "2"
+         Invoke-WebRequest -Uri "https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Archive/GridSwCert-Windows_2020_04.cert" -OutFile "$Env:PUBLIC\Documents\GridSwCert.txt"
+         New-ItemProperty -Path "HKLM:\SOFTWARE\NVIDIA Corporation\Global\GridLicensing" -Name "FeatureType" -PropertyType "DWord" -Value "0"
+         New-ItemProperty -Path "HKLM:\SOFTWARE\NVIDIA Corporation\Global\GridLicensing" -Name "IgnoreSP" -PropertyType "DWord" -Value "1"
+         cd "C:\Program Files\NVIDIA Corporation\NVSMI"
+         .\nvidia-smi -ac "5001,1590"
+         Write-host "Updating your Screen resolution"
+         Set-DisplayResolution -Width $Screenwidth -Height $Screenheight -Force
+        }
+    N {
+        Return
+        }
+    }
+
+#Create Scripts to set resolution each time at startup
+Write-host "Creating Scripts Directory in C:\Users\Administrator\Documents\Scripts"
+New-Item -Path 'C:\Users\Administrator\Documents\Scripts' -ItemType Directory
+New-Item C:\Users\Administrator\Documents\Scripts\SetResolution.ps1
+Set-Content C:\Users\Administrator\Documents\Scripts\SetResolution.ps1 'Set-DisplayResolution -Width 1920 -Height 1080 -Force'
+New-Item C:\Users\Administrator\Documents\Scripts\Startup.bat
+Set-Content C:\Users\Administrator\Documents\Scripts\Startup.bat '@ECHO OFF
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\Users\Administrator\Documents\Scirpts\SetResolution.ps1 >> "%TEMP%\StartupLog.txt" 2>&1'
+Write-host "Created Script files SetResolutions.ps1 and Startup.bat"
+Write-host "Creating a task schedule to run startup.bat at every Windows Startup."
+
+$A = New-ScheduledTaskAction -Execute "C:\Users\Administrator\Documents\Scripts\Startup.bat"
+$T = New-ScheduledTaskTrigger -AtLogon
+$P = New-ScheduledTaskPrincipal "Administrator"
+$S = New-ScheduledTaskSettingsSet 
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+Register-ScheduledTask -TaskName "SetResolution" -InputObject $D 
+
+Write-host "New Task 'SetResolution' created."
+
+<# Install Visual C++ Redistributable 2015 32Bit (necessary for installing Origin)#>
+Write-host "Installing Visual C++ Redistributable 2015 32Bit (prerequisite for Origin)"
+$VCRedistLocalPath = "$ENV:UserProfile\Downloads\vc_redist.x86"  
+(New-Object System.Net.WebClient).DownloadFile($VCRedistLocation, "$VCRedistLocalPath.exe")  
+Start-Process -Filepath "$VCRedistLocalPath.exe" -ArgumentList '/S /quiet /norestart' -Wait
+Write-host "Visual C++ Installed"
+
+
+<# Download and Install TightVNC Server #>
+Write-host "Installing TightVNC Server"
+$TightVNCLocalPath = "$ENV:UserProfile\Downloads\TightVNCServer"  
+(New-Object System.Net.WebClient).DownloadFile($TightVNCLocation, "$TightVNCLocalPath.msi")  
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\Users\Administrator\Downloads\TightVNCServer.msi /quiet'
+$ipinfo = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
+Write-host "TightVNC Server installed. Connect to VNC Server via IP Address: "$ipinfo
+
+<# Download and Install Steam #>
+Write-host "Installing Steam"
+$SteamLocalPath = "$ENV:UserProfile\Downloads\Steam"  
+(New-Object System.Net.WebClient).DownloadFile($SteamLocation, "$SteamLocalPath.exe") 
+Start-Process -Filepath "$SteamLocalPath.exe" -ArgumentList '/S' -Wait
+Write-host "Steam installed"
+
+<# Download Origin #>
+"Installing Origin - this is a manual setup process. Please take over once the Origin Window opens"
+$OriginLocalPath = "$ENV:UserProfile\Downloads\Origin" 
+(New-Object System.Net.WebClient).DownloadFile($OriginLocation, "$OriginLocalPath.exe") 
+Start-Process -Filepath "$OriginLocalPath.exe"
---
+++ Installation_part1.ps1
@@ -0,0 +1,51 @@
+﻿<# Set Variables for Download URLs #>
+$ScriptWebArchive = "https://parsecscripts.s3.amazonaws.com/Parsec-Cloud-Preparation-Tool-rt.zip" 
+
+<# Set AWS Credentials #>
+Write-host "Please enter your AWS IAM Credntials:"
+$AwsAccessKey = Read-host "Access Key"
+$AwsSecretKey = Read-host "Secret Key" -AsSecureString
+
+<# Set Windows Administrator PAssword #>
+$Password = Read-Host "Enter a new Windows Password for Administrator" -AsSecureString
+$UserAccount = Get-LocalUser -Name "Administrator"
+$UserAccount | Set-LocalUser -Password $Password
+
+<# Run Parsec Installation Scripts #>
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+$LocalArchivePath = "$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool"  
+(New-Object System.Net.WebClient).DownloadFile($ScriptWebArchive, "$LocalArchivePath.zip")  
+Expand-Archive "$LocalArchivePath.zip" -DestinationPath $LocalArchivePath -Force  
+CD $LocalArchivePath\Parsec-Cloud-Preparation-Tool-master\ | powershell.exe .\loader.ps1
+
+Get-Job | Wait-Job
+
+<# Install AWS Tools for Powershell, set credentials, and download NVidia Gaming driver from S3 #>
+Install-Module -Name AWS.Tools.Installer -SkipPublisherCheck -Force
+
+Get-Job | Wait-Job
+
+Set-AWSCredential `
+                 -AccessKey $AwsAccessKey `
+                 -SecretKey $AwsSecretKey `
+                 -StoreAs default
+
+Get-Job | Wait-Job
+
+$Bucket = "nvidia-gaming"
+$KeyPrefix = "windows/latest"
+$LocalPath = "$home\Desktop\NVIDIA"
+$Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1
+foreach ($Object in $Objects) {
+    $LocalFileName = $Object.Key
+    if ($LocalFileName -ne '' -and $Object.Size -ne 0) {
+        $LocalFilePath = Join-Path $LocalPath $LocalFileName
+        Copy-S3Object -BucketName $Bucket -Key $Object.Key -LocalFile $LocalFilePath -Region us-east-1
+        $NewFileName = $LocalFileName + "_unzipped"
+        Expand-Archive -LiteralPath $LocalFilePath -DestinationPath "$home\Desktop\GraphicsDriver"
+    }
+}
+
+Write-Host "Please wait before starting the NVidia Installation until Parsec has finished installing"
+<# Launch Graphics Driver Installation #>
+Start-Process -Filepath "C:\Users\Administrator\Desktop\GraphicsDriver\Windows\445.87_grid_vgaming_win10_64bit_international_whql.exe" -Wait
